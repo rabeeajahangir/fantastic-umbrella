fcd4c4f63bbea1e1dc147aeb97ef0cbb0feadc5e
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-07-28 23:13:54 -0400

    changed to index, delete product not working

--- models/index.js
+++ models/index.js
@@ -27,12 +27,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
 })
 module.exports = { Category, Product, ProductTag, Tag};
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -60,14 +60,18 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+    console.log(req.body);
   Product.create(req.body,
     {
       product_name: req.body.product_name,
       price: req.body.price,
       stock: req.body.stock,
-      tagIds: req.body.tagIds
+      category_id: req.body.tagIds,
+      tagIds: []
     }
   )
+  
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
